5e82cd1f96b7f4bbaa84f04d28e039892049e031
southboy12 <menkachi85@gmail.com>
2025-04-06 23:32:08 +0100

    adjusted text size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ export default function Header() {
     return (
         <nav className="header bg-cover bg-center bg-blend-multiply bg-black/70 px-8 py-20">
             <ul className="flex justify-between items-center">
-                <li className="text-[42px] text-[#FFFFFF] font-[800]"><Link to="/">Find your film</Link></li>
-                <li className="text-[14px] text-[#FFFFFF] font-[700]"><Link to="wishlist">My Watchlist</Link></li>
+                <li className="text-[30px] text-[#FFFFFF] font-[800] md:text-[10px]"><Link to="/">Find your film</Link></li>
+                <li className="text-[10px] text-[#FFFFFF] font-[700]"><Link to="wishlist">My Watchlist</Link></li>
             </ul>
         </nav>
     )
